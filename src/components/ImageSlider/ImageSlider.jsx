8bcdf4bf28edd9d866e5e111beedfadb14e1f14c
elaaasri <anas_elasri@outlook.fr>
2026-01-02 22:06:03 +0100

    refactor(ImageSlider): convert ImageSlider styles to css modules

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -3,25 +3,24 @@ import { Navigation, Pagination, Zoom, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/zoom";
 import "swiper/css/autoplay";
+import styles from "./ImageSlider.module.css";
 
 // display product images with a slider using swiper library :
 const ImageSlider = ({ images, title }) => {
   return (
     <Swiper
-      className="images-slider-container"
+      className={styles.container}
       slidesPerView={1}
-      modules={[Navigation, Pagination, Zoom, Autoplay]}
+      modules={[Navigation, Pagination, Autoplay]}
       navigation
       pagination={{ clickable: true }}
       loop={true}
-      zoom={true}
       autoplay={{ delay: 2500, disableOnInteraction: true }}
     >
-      {images.map((image) => (
-        <SwiperSlide>
-          <div className="swiper-zoom-container">
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <div className={styles.imgWrapper}>
             <img src={image} alt={title + " product image"} />
           </div>
         </SwiperSlide>
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import { useParams, useOutletContext } from "react-router";
 import QuantityButton from "../../components/QuantityButton";
-import ImageSlider from "../../components/ImageSlider";
 import styles from "./ProductPage.module.css";
 import { Link } from "react-router";
 import { useLocation } from "react-router";
+import ImageSlider from "../../components/ImageSlider/ImageSlider";
 
 const ProductPage = () => {
   const { products, setCartItems } = useOutletContext();
   const { category, title } = useParams();
   const filteredProduct = getProductByTitle(products, title);
   const [productQuantity, setProductQuantity] = useState(1);
-  const location = useLocation();
+  // const location = useLocation();
 
-  const navPaths = location.pathname
-    .split("/")
-    .filter(Boolean)
-    .map(decodeURIComponent);
+  // const navPaths = location.pathname
+  //   .split("/")
+  //   .filter(Boolean)
+  //   .map(decodeURIComponent);
   // .map((path, index) => (index == 2 ? decodeURIComponent(path) : path));
 
   const handleAddToCart = () => {
@@ -26,6 +26,7 @@ const ProductPage = () => {
       ...Array(productQuantity).fill(filteredProduct).flat(),
     ]);
   };
+  console.log(filteredProduct);
 
   return (
     <div className={styles.container}>
@@ -36,31 +37,48 @@ const ProductPage = () => {
         &nbsp; / &nbsp;
         <Link>{title.toUpperCase()}</Link>
       </nav>
-      <div className={styles.product}>
+      <div className={styles.productContainer}>
         {filteredProduct.map(({ title, price, description, images, id }) => {
           const imgsLength = images.length;
+
           return (
+            <>
+              <div className={styles.imgWrapper}>
+                {imgsLength === 1 ? (
+                  <img
+                    className={styles.img}
+                    src={images[0]}
+                    alt={title + " product image"}
+                  />
+                ) : (
+                  <ImageSlider images={images} title={title} />
+                )}
+              </div>
+
+              <div className={styles.productInfos}>B</div>
+            </>
+
             // classname
-            <div key={id}>
-              {imgsLength === 1 ? (
-                <img
-                  className={styles.img}
-                  src={images[0]}
-                  alt={title + " product image"}
-                />
-              ) : (
-                <ImageSlider images={images} title={title} />
-              )}
-              <QuantityButton
-                quantity={productQuantity}
-                setQuantity={setProductQuantity}
-              />
-              <button onClick={handleAddToCart}>Add to Cart</button>
-              <div>{title}</div>
-              <div>{price}</div>
-              <div>{description}</div>
-              <h2>image length : {images.length}</h2>
-            </div>
+            // <div key={id}>
+            //   {imgsLength === 1 ? (
+            //     <img
+            //       className={styles.img}
+            //       src={images[0]}
+            //       alt={title + " product image"}
+            //     />
+            //   ) : (
+            //     <ImageSlider images={images} title={title} />
+            //   )}
+            //   <QuantityButton
+            //     quantity={productQuantity}
+            //     setQuantity={setProductQuantity}
+            //   />
+            //   <button onClick={handleAddToCart}>Add to Cart</button>
+            //   <div>{title}</div>
+            //   <div>{price}</div>
+            //   <div>{description}</div>
+            //   <h2>image length : {images.length}</h2>
+            // </div>
           );
         })}
       </div>
