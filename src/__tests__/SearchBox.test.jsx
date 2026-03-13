2b1bc86b5116ac7ed7e6fc88c6358b60d5e1bfd2
elaaasri <anas_elasri@outlook.fr>
2026-03-13 14:07:43 +0000

    test(search-box): renders search result test

--- src/__tests__/SearchBox.test.jsx
+++ src/__tests__/SearchBox.test.jsx
@@ -4,6 +4,7 @@ import userEvent from "@testing-library/user-event";
 import { createMemoryRouter, MemoryRouter, RouterProvider } from "react-router";
 import routes from "../app/routes";
 import SearchBox from "../components/SearchBox/SearchBox";
+import { exp } from "three/tsl";
 
 describe("Search Box", () => {
   beforeEach(() => {
@@ -15,8 +16,8 @@ describe("Search Box", () => {
 
   it("open and close search box", async () => {
     const searchIcon = screen.getByTestId("search-icon");
-
     await userEvent.click(searchIcon);
+
     expect(await screen.findByText(/SEARCH OUR ITEMS/i)).toBeInTheDocument();
 
     const closeIcon = screen.getByTestId("close-icon");
@@ -27,18 +28,17 @@ describe("Search Box", () => {
     });
   });
 
-  // it("renders search result", async () => {
-  //   const products = [{ id: 1, title: "kiwi", price: "2.49$" }];
+  it("renders search result", async () => {
+    const products = [{ id: 1, title: "kiwi", price: 2.49 }];
+    render(
+      <MemoryRouter>
+        <SearchBox setShowSearchBox={vi.fn()} products={products} />
+      </MemoryRouter>,
+    );
 
-  //   render(
-  //     <MemoryRouter>
-  //       <SearchBox setShowSearchBox={vi.fn()} products={products} />
-  //     </MemoryRouter>,
-  //   );
+    const searchInput = screen.getByTestId("search-input");
+    await userEvent.type(searchInput, "kiwi");
 
-  //   const searchInput = screen.getByPlaceholderText(/search products ../i);
-  //   await userEvent.type(searchInput, "kiwi");
-
-  //   expect(await screen.findByText("2.49$")).toBeInTheDocument();
-  // });
+    expect(await screen.findByText("2.49$")).toBeInTheDocument();
+  });
 });
